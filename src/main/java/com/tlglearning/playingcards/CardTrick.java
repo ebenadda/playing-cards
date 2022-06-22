aae1bbedac6aa24f3b78e80fdd33784ea28d8bea
ebenadda <ebaddae@gmail.com>
2022-06-22 18:44:04 -0400

    Introduced Lambda expressions

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -68,29 +68,12 @@ public class CardTrick {
             }
         }
 
-        class DisplayComparator implements Comparator<Card> {
-
-            @Override
-            public int compare(Card card1, Card card2) {
-                int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
-                comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-                comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-                return comparison;
-            }
-
-        }
-
-      Comparator<Card> comparator = new Comparator<>() {
-
-          @Override
-          public int compare(Card card1, Card card2) {
-              int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
-              comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-              comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-              return comparison;
-          }
-
-      };
+        Comparator<Card> comparator = (card1, card2) -> {
+            int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
+            comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+            comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+            return comparison;
+        };
 
         Collections.sort((LinkedList<Card>) blackPile, comparator);
         Collections.sort((LinkedList<Card>) redPile, comparator);
